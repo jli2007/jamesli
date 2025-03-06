c083e4cd55e73f1c58fa19893055430fb48c4d21
JLi2007 <LiJames9532@gmail.com>
2025-03-05 20:34:13 -0500

    can't rush greatness

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import LoadingBar from "react-top-loading-bar";
+import jam from "../../assets/jam.png";
 
-// import { CiLinkedin } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -15,12 +16,12 @@ export default function Home() {
         currentProgress += 12;
         setProgress(currentProgress);
       } else {
-        clearInterval(interval); 
-        setIsLoaded(true); 
+        clearInterval(interval);
+        setIsLoaded(true);
       }
-    }, 300); 
+    }, 300);
 
-    return () => clearInterval(interval); 
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -36,8 +37,9 @@ export default function Home() {
           progress={progress}
           onLoaderFinished={() => setProgress(0)}
         />
-        <div className="relative flex items-center justify-center">
-          <h1 className="text-white text-xl crg">can't rush greatness.</h1>
+
+        <div className="relative flex justify-center items-center">
+          <h1 className="text-white text-xl crg my-3">can't rush greatness.</h1>
         </div>
       </div>
 
@@ -47,29 +49,62 @@ export default function Home() {
             james siyuan li
           </h1>
           <div className="grid grid-flow-row md:grid-flow-col grid-rows-2 gap-1 h-[90vh]">
-            <div className="row-span-6 col-span-5 md:w-auto w-screen py-3 px-7 bg-midBeige1 m-1 rounded-lg underline">
-              img
-              {/* <CiLinkedin style={{border: "1px solid red", width:"10vw", height:"10vh"}}/> */}
+            <div className="row-span-6 col-span-5 md:w-auto w-screen py-3 px-7 bg-midBeige1 m-1 rounded-lg">
+              <span className="italic text-xl drop-shadow-[2px_2px_3px_rgba(0,0,0,0.5)]">can't rush greatness.</span>
+              <div className="text-xl h-[95%] flex items-end">
+                <div className="flex flex-col gap-5">
+                <h1>hey i'm james.</h1>
+                <h1>· founder @ <span className="underline">neodev</span></h1>
+                <h1>· prev. intern @ <span className="underline">lakes software</span></h1>
+                <h1>· prev. junior software dev @ <span className="underline">tauria</span></h1>
+                </div>
+              </div>
             </div>
+            <img
+              src={jam}
+              className="absolute md:-left-5 md:w-135 h-auto opacity-98 rounded-xl"
+              style={{
+                WebkitMaskImage:
+                  "radial-gradient(circle, rgba(0,0,0,1) 40%, rgba(0,0,0,0) 100%)",
+              }}
+            />
 
-            <div className="row-span-1 col-span-3 md:w-auto w-screen grid grid-cols-3 gap-2 m-1">
-              <div className="col-span-2 py-3 px-7 rounded-lg bg-darkBeige2 text-lightBeige underline">
+            <div className="row-span-1 col-span-4 md:w-auto w-screen grid grid-cols-3 gap-2 m-1">
+              <div className="col-span-2 py-3 px-7 rounded-lg bg-darkBeige2 text-lightBeige italic">
                 linkedin
               </div>
-              <div className="py-3 px-7 rounded-lg bg-midBeige1 underline">blog</div>
+              <div className="py-3 px-7 rounded-lg bg-midBeige1 italic">
+                blog
+              </div>
             </div>
 
-            <div className="col-span-3 row-span-1 md:w-auto w-screen py-3 px-7 bg-midBeige1 m-1 rounded-lg underline">
-              desc
+            <div className="col-span-4 row-span-1 md:w-auto w-screen py-3 px-7 bg-midBeige1 m-1 rounded-lg">
+              <span className="italic z-10">
+                programmer | student | athlete
+              </span>
             </div>
-            <div className="col-span-3 row-span-4 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-darkBeige1 text-midBeige1 underline">
-              smth design
+
+            <div className="col-span-3 row-span-4 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-darkBeige1 text-midBeige1 underline"></div>
+            <div className="col-span-6 row-span-3 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-midBeige2">
+              <div className="flex w-full items-center">
+                <div className="flex justify-start w-full">
+                  <span className="italic">hello world</span>
+                </div>
+                <div className="justify-end flex w-full">
+                  <button className="cursor-pointer">
+                    <a
+                      href="https://www.linkedin.com/in/james-li-a81004275/"
+                      target="_blank"
+                    >
+                      {" "}
+                      <CiLinkedin className="w-[5vw] h-[5vh]" />
+                    </a>
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="col-span-3 row-span-5 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-midBeige2 underline">
+            <div className="col-span-5 row-span-2 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-midBeige1 italic">
               projects
-            </div>
-            <div className="col-span-3 row-span-1 md:w-auto w-screen py-3 px-7 m-1 rounded-lg bg-midBeige1 underline">
-              profiles
             </div>
           </div>
         </div>
