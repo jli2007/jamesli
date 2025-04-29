a9656d4c61f68f8c66b31a646bb0c6462e3d200d
JLi2007 <LiJames9532@gmail.com>
2025-04-29 13:44:03 -0400

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import CommandPalette from "./components/Cmd";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={"font-playfair"}>
         <div>{children}</div>
         <CommandPalette />
+        <GoogleAnalytics gaId="G-T54T8RQLW5" />
       </body>
     </html>
   );
